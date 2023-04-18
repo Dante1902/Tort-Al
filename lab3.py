9236a4733e6d4c0dcd530b0c658770a6f6ecca95
Dante1902 <128978419+Dante1902@users.noreply.github.com>
2023-04-18 13:01:25 +0800

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,6 @@
+import requests
+
+template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
+b = requests.get(Template) # получение запроса
+b.encoding="windows-1251" # кодировка
+print(b.text) # вывод на экран
