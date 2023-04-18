1457df07f2cadca968b586f62417b8a33770335c
Dante1902 <128978419+Dante1902@users.noreply.github.com>
2023-04-18 14:10:18 +0800

    Update laba4.py

--- laba4.py
+++ laba4.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup # библиотека Python для извлечения данных из файлов HTML и XML
 import requests
 
-Template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
-a = requests.get(Template) # получение запроса
-a.encoding="windows-1251" # кодировка
-print(a.text) # вывод на экран
-if a == 404: # если ошибка
+Raspisanie = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
+b = requests.get(Raspisanie) # получение запроса
+b.encoding="windows-1251" # кодировка
+print(b.text) # вывод на экран
+if b == 404: # если ошибка
 	print ('Not Found')
 else: 
-	html_code = a.text
-	bs =BeautifulSoup(html_code,"html.parser") #парсинг html документа
-	Lesson = bs.findAll('td') # td - столбец
+	HTML_cd = b.text
+	ls =BeautifulSoup(HTML_cd,"html.parser") #парсинг html документа
+	Lesson = ls.findAll('td') # td - столбец
 	
 	file = open ("Timetable.txt", "w") # запись в файл
 	for item in Lesson:
@@ -21,7 +21,7 @@ else:
 	for i in range(104):
 		line.pop(0)
 	
-	f = open ("Timetable.txt", "w")
+	a = open ("Timetable.txt", "w")
 	for i in line:
-		f.write(i)
-	f.close()
+		a.write(i)
+	a.close()
