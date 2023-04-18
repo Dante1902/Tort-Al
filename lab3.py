8d5f8d98e959ecacf5cfee9643af241141806377
Dante1902 <128978419+Dante1902@users.noreply.github.com>
2023-04-18 13:22:33 +0800

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,6 +1,6 @@
 import requests
 
-Template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
-b = requests.get(Template) # получение запроса
+Raspisanie = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
+b = requests.get(Raspisanie) # получение запроса
 b.encoding="windows-1251" # кодировка
 print(b.text) # вывод на экран
